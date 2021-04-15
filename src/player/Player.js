1081b8b9fad221f86e31bafc4fdf09c96f44220f
builder_247 <14019974+builder-247@users.noreply.github.com>
2021-04-15 18:15:34 +0300

    fix lint

--- src/player/Player.js
+++ src/player/Player.js
@@ -200,7 +200,7 @@ class Player {
           xpForNext,
         } = util.getSlayerLevel({ claimedLevels: Object.keys(claimed_levels), xp }, name);
         return {
-          claimed_levels: Object.keys(claimed_levels).filter(level => !level.endsWith('_special')).length,
+          claimed_levels: Object.keys(claimed_levels).filter((level) => !level.endsWith('_special')).length,
           xp,
           xp_for_next: Math.max(xpForNext - xp, 0),
           kills_tier: {
